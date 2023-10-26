9c964fdd820378f1a04cb32dee4f154d54c7e60b
Andrey Karpuk <146330533+AKproAndrey709@users.noreply.github.com>
2023-10-26 10:23:15 +0300

    Ввод по буквам

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Bot for Yandex
 // @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Bot
+// @version      0.2
+// @description  try to take over the world!
 // @author       Karpuk Andrey
 // @match        https://ya.ru/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
@@ -13,21 +13,33 @@ let links = document.links;
 let searchBtn = document.querySelector(".mini-suggest__button");
 let input = document.getElementById("text");
 let keywords = [
-  "Инструменты",
-  "Ручной инструмент",
+  "Инструменты купить",
+  "Немецкое качество",
   "Бензопилы",
+  // "10 самых популярных шрифтов Google",
+  // "Отключение редакций и ревизий",
+  // "Вывод произвольных типов записей и полей",
 ];
-
 let keyword = keywords[getRandom(0, keywords.length)];
-
+// Работаем на главной странице
 if (input !== null) {
-  input.value = keyword;
-  searchBtn.click();
+  let i = 0;
+  let timerId = setInterval(() => {
+    input.value += keyword[i];
+    i++;
+    if (i == keyword.length) {
+      clearInterval(timerId);
+      searchBtn.click();
+    }
+  }, 500);
+
+  // Работаем на странице поисковой выдачи
+  // input.value = keyword;
 } else {
   for (let i = 0; i < links.length; i++) {
     if (links[i].href.includes("vseinstrumenti.ru")) {
       let link = links[i];
-      link.setAttribute('target', '_self');
+      link.setAttribute("target", "_self");
       console.log("FIND " + link);
       link.click();
       break;
