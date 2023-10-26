33846c9ce5c6ea8b24979f92c069ee02053b651f
Andrey Karpuk <146330533+AKproAndrey709@users.noreply.github.com>
2023-10-26 10:28:32 +0300

    Ввод по буквам

--- Bot/Bot.js
+++ Bot/Bot.js
@@ -1,52 +1,52 @@
-// ==UserScript==
-// @name         Bot for Yandex
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  try to take over the world!
-// @author       Karpuk Andrey
-// @match        https://ya.ru/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
-// @grant        none
-// ==/UserScript==
+			// ==UserScript==
+			// @name         Bot for Yandex
+			// @namespace    http://tampermonkey.net/
+			// @version      0.1
+			// @description  try to take over the world!
+			// @author       Karpuk Andrey
+			// @match        https://ya.ru/*
+			// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+			// @grant        none
+			// ==/UserScript==
 
-let links = document.links;
-let searchBtn = document.querySelector(".mini-suggest__button");
-let input = document.getElementById("text");
-let keywords = [
-  "Инструменты купить",
-  "Немецкое качество",
-  "Бензопилы",
-  // "10 самых популярных шрифтов Google",
-  // "Отключение редакций и ревизий",
-  // "Вывод произвольных типов записей и полей",
-];
-let keyword = keywords[getRandom(0, keywords.length)];
-// Работаем на главной странице
-if (input !== null) {
-  let i = 0;
-  let timerId = setInterval(() => {
-    input.value += keyword[i];
-    i++;
-    if (i == keyword.length) {
-      clearInterval(timerId);
-      searchBtn.click();
-    }
-  }, 500);
+			let links = document.links;
+			let searchBtn = document.querySelector(".mini-suggest__button");
+			let input = document.getElementById("text");
+			let keywords = [
+				"Инструменты купить",
+				"Немецкое качество",
+				"Бензопилы",
+				// "10 самых популярных шрифтов Google",
+				// "Отключение редакций и ревизий",
+				// "Вывод произвольных типов записей и полей",
+			];
+			let keyword = keywords[getRandom(0, keywords.length)];
+			// Работаем на главной странице
+			if (input !== null) {
+				let i = 0;
+				let timerId = setInterval(() => {
+					input.value += keyword[i];
+					i++;
+					if (i == keyword.length) {
+						clearInterval(timerId);
+						searchBtn.click();
+					}
+				}, 500);
 
-  // Работаем на странице поисковой выдачи
-  // input.value = keyword;
-} else {
-  for (let i = 0; i < links.length; i++) {
-    if (links[i].href.includes("vseinstrumenti.ru")) {
-      let link = links[i];
-      link.setAttribute("target", "_self");
-      console.log("FIND " + link);
-      link.click();
-      break;
-    }
-  }
-}
+				// Работаем на странице поисковой выдачи
+				// input.value = keyword;
+			} else {
+				for (let i = 0; i < links.length; i++) {
+					if (links[i].href.includes("vseinstrumenti.ru")) {
+						let link = links[i];
+						link.setAttribute("target", "_self");
+						console.log("FIND " + link);
+						link.click();
+						break;
+					}
+				}
+			}
 
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min) + min);
-}
+			function getRandom(min, max) {
+				return Math.floor(Math.random() * (max - min) + min);
+			}
